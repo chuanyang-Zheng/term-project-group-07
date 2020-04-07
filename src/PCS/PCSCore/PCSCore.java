e2d9bd62e2225fa33dd727b32e8c1c99de6aea7e
Pan Feng <1760495180@qq.com>
2020-04-07 19:15:03 +0800

    Debug Paymachine

--- src/PCS/PCSCore/PCSCore.java
+++ src/PCS/PCSCore/PCSCore.java
@@ -175,7 +175,7 @@ public class PCSCore extends AppThread {
                 log.warning(id + ": Find Invalid Ticket [" + Integer.parseInt(tmp[1] + "] When Calculate Fee"));
             } else {
                 Ticket targetTicket = ticketList.get(ticketIndexInTicketArrayList);//get ticket
-                if(ticketList.get(ticketIndexInTicketArrayList).getPayMachineID().equals("")) {
+                if(ticketList.get(ticketIndexInTicketArrayList).getPayMachineID().equals("")) { // the 3rd parameter mapping
                     msg.getSenderMBox().send(new Msg(id, mbox, Msg.Type.TicketFee, tmp[0] + "," + tmp[1] + "," + Float.toString(targetTicket.calculateFee(calculateFeeCoefficient)) + "," + Long.toString(targetTicket.getEnterTime())));
                     targetTicket.setExitInformation(exitTimeCoefficient, msg.getSender(), calculateFeeCoefficient);
                 }//send corresponding fee
